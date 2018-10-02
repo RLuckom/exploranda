226f597b0dfbdf07dde86b4382f1859f4fedcc03
Raphael Luckom <raphaelluckom@gmail.com>
2018-10-02 01:54:57 -0400

    graph the metric

--- examples/instancesJsonGcp.js
+++ examples/instancesJsonGcp.js
@@ -1,16 +1,19 @@
+const _ = require('lodash');
 const exploranda = require('../lib/reporter');
 const {instancesBuilder} = exploranda.dataSources.google.compute;
 const {timeSeriesBuilder} = exploranda.dataSources.google.stackdriverMonitoring;
+const {logEntriesBuilder} = exploranda.dataSources.google.stackdriverLogging;
 
-/* USAGE: node ./instancesJsonGcp.js <path-to-service-account-credential-json> <name-of-instance>
+/* USAGE: node ./instancesJsonGcp.js <path-to-service-account-credential-json> <name-of-instance> <projectId>
  *
- * This example lists instances and gets disk write ops for a named instance passed as an argument.
+ * This example lists instances and graphs disk write ops for a named instance passed as an argument.
  * It requires a JSON file of service account credentials as described in 
  * https://cloud.google.com/docs/authentication/production
  */
 
 const keyFilename = process.argv[2];
 const instanceName = process.argv[3];
+const projectName = process.argv[4];
 
 const apiConfig = {keyFilename};
 
@@ -31,9 +34,45 @@ function getReport() {
   const reporter = new exploranda.Reporter();
   reporter.setSchemas({
     dependencies: {
-      instances: instancesBuilder(apiConfig, 'root-217800', ['us-east1-b', 'us-east1-d']),
-      ts: timeSeriesBuilder(apiConfig, 'root-217800', sdfilter, sdAgg, interv)
+      instances: instancesBuilder(apiConfig, projectName, ['us-east1-b', 'us-east1-d']),
+      diskWriteOps: timeSeriesBuilder(apiConfig, projectName, sdfilter, sdAgg, interv)
     },
+    transformation: {
+      'Instances': {
+        source: 'instances',
+        tableBuilder: (instances) => _.concat([null], _.map(instances, 'name'))
+      },
+      [`${instanceName} Disk Write Ops`]: {
+        source: 'diskWriteOps',
+        tableBuilder: (diskWriteOps) => {
+          points = _.reverse(diskWriteOps[0].points);
+          return [{
+            title: `${instanceName} Disk Write Ops`,
+            style: {line: 'red'},
+            x: _.map(points, 'interval.startTime'),
+            y: _.map(points, 'value.doubleValue')
+          }];
+        },
+      }
+    },
+    display: {
+      markdowns: {
+        'Instances': {
+          column: 0,
+          row: 0,
+          rowSpan: 12,
+          columnSpan: 3
+        }
+      },
+      lines: {
+        [`${instanceName} Disk Write Ops`]: {
+          column: 3,
+          row: 0,
+          rowSpan: 12,
+          columnSpan: 9
+        },
+      }
+    }
   });
   reporter.execute((e, r) => console.log(JSON.stringify(r, null, 2)));
 }
