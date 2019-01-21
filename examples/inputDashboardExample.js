866c88492f6b1b4508638a1f3c95382be504d93e
Raphael Luckom <raphaelluckom@gmail.com>
2019-01-21 11:12:17 -0500

    time input

--- examples/inputDashboardExample.js
+++ examples/inputDashboardExample.js
@@ -2,55 +2,33 @@ const exploranda = require('../lib/reporter');
 const _ = require('lodash');
 const moment = require('moment');
 
-const commit = process.argv[2];
-
-const dependencies = {
-  commits: {
-    accessSchema: exploranda.dataSources.github.commits.accessSchema,
-    params: {
-      owner: {value: 'RLuckom'},
-      repo: {value: 'exploranda'}
-    }
-  },
-  releases: {
-    accessSchema: exploranda.dataSources.npm.releases.accessSchema,
-    params: {
-      package: {
-        input: 'library',
-        formatter: ({library}) => library
-      }
-    }
-  },
-};
-
 exploranda.widgetDashboard({
-  dataDependencies: dependencies,
+  dataDependencies: {},
   inputs: {
-    library: 'exploranda'
+    library: 'exploranda',
+    time: 'RLuckom/exploranda'
   },
   display: {
     widgets: {
-      releases: {
-        title: '%library releases %time',
-        source: 'releases',
-        usesInputKeys: 'library',
-        displayType: 'markdown',
-        position: {
-          column: 0,
-          row: 0,
-          rowSpan: 12,
-          columnSpan: 6
-        },
-        transformation: ({releases}) => _.concat([null], _.map(releases[0].versions, 'version'))
-      },
-      input: {
-        title: 'input',
-        displayType: 'textBox',
+      libraryInput: {
+        title: 'library',
+        displayType: 'timeBox',
         inputKeys: 'library',
         position: {
           column: 6,
           row: 0,
-          rowSpan: 12,
+          rowSpan: 2,
+          columnSpan: 6
+        },
+      },
+      userInput: {
+        title: 'time',
+        displayType: 'timeBox',
+        inputKeys: 'repo',
+        position: {
+          column: 6,
+          row: 2,
+          rowSpan: 2,
           columnSpan: 6
         },
       }
